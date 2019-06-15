3db81253fc0dda1b11f8ae7b9d3a9daed97a00ba
mio-158cm <hayakawan18@gmail.com>
2019-06-15 18:53:08 +0900

    foreign_key:true追加

--- db/migrate/20190615050454_create_transactions.rb
+++ db/migrate/20190615050454_create_transactions.rb
@@ -1,9 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      t.references :item
-      t.references :buyer
-      t.references :seller
+      t.references :item, foreign_key:true
+      t.references :buyer, foreign_key:true
+      t.references :seller, foreign_key:true
 
       t.timestamps
     end
