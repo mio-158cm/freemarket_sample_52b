774dbac4d2f30f6375aa718f92a8a599db1bf152
mio-158cm <hayakawan18@gmail.com>
2019-06-15 19:22:13 +0900

    change-schema

--- db/schema.rb
+++ db/schema.rb
@@ -169,6 +169,4 @@ ActiveRecord::Schema.define(version: 2019_06_15_050618) do
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "users"
-  add_foreign_key "profiles", "users"
 end
