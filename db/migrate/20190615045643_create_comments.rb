7a45231d3c62e131bc86ba5a5a1dd584688fbbc4
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-06-17 08:05:29 +0900

    comment out foreign key of each table

--- db/migrate/20190615045643_create_comments.rb
+++ db/migrate/20190615045643_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :text
-      t.references :user, foreign_key:true
-      t.references :item, foreign_key:true
+      # t.references :user, foreign_key:true
+      # t.references :item, foreign_key:true
 
       t.timestamps
     end
