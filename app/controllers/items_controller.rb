44a28386141db9401d662c2406fb17c1b56ea459
yui4211 <sunba.raria.fisom428@gmail.com>
2019-06-18 10:36:22 +0900

    update top

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    @lady_items = Item.where(category_id: Category.find(1).subtree_ids).order(created_at: "DESC").limit(4)
+    @man_items = Item.where(category_id: Category.find(200).subtree_ids).order(created_at: "DESC").limit(4)
+    @kids_items = Item.where(category_id: Category.find(345).subtree_ids).order(created_at: "DESC").limit(4)
+    @beauty_items = Item.where(category_id: Category.find(793).subtree_ids).order(created_at: "DESC").limit(4)
   end
 
   def new
