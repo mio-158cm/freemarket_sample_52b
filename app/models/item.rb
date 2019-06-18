e7ef422a77ef7ac5b9ee9d7f9d41236095a42709
mio-158cm <hayakawan18@gmail.com>
2019-06-18 16:55:05 +0900

    optional: true追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  belongs_to :brand
-  belongs_to :size
-  belongs_to :category
+  belongs_to :user, optional: true
+  belongs_to :brand, optional: true
+  belongs_to :size, optional: true
+  belongs_to :category, optional: true
   has_many :likes
   has_many :comments
   has_many :images
