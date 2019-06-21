a0bf77436911dddd0b3fd4ab62a00d895f44735c
yui4211 <sunba.raria.fisom428@gmail.com>
2019-06-21 10:35:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
   def index
     @lady_items = Item.includes(:images).where(category_id: Category.find(1).subtree_ids).order(created_at: "DESC").limit(4)
-    @man_items = Item.includes(:images).where(category_id: Category.find(200).subtree_ids).order(created_at: "DESC").limit(4)
-    @kids_items = Item.includes(:images).where(category_id: Category.find(345).subtree_ids).order(created_at: "DESC").limit(4)
-    @beauty_items = Item.includes(:images).where(category_id: Category.find(793).subtree_ids).order(created_at: "DESC").limit(4)
+    @man_items = Item.includes(:images).where(category_id: Category.find(2).subtree_ids).order(created_at: "DESC").limit(4)
+    @kids_items = Item.includes(:images).where(category_id: Category.find(3).subtree_ids).order(created_at: "DESC").limit(4)
+    @beauty_items = Item.includes(:images).where(category_id: Category.find(7).subtree_ids).order(created_at: "DESC").limit(4)
     @chanel = Item.includes(:images).where(brand_id: 1).order(created_at: "DESC").limit(4)
-    @vuitton = Item.includes(:images).where(brand_id: 3).order(created_at: "DESC").limit(4)
-    @supreme = Item.includes(:images).where(brand_id: 4).order(created_at: "DESC").limit(4)
-    @nike = Item.includes(:images).where(brand_id: 2).order(created_at: "DESC").limit(4)
+    @vuitton = Item.includes(:images).where(brand_id: 2).order(created_at: "DESC").limit(4)
+    @supreme = Item.includes(:images).where(brand_id: 3).order(created_at: "DESC").limit(4)
+    @nike = Item.includes(:images).where(brand_id: 4).order(created_at: "DESC").limit(4)
   end
 
   def new
