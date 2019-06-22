aedc9746ae658bc3a000c653ad003558032d7cda
Emi Yamamoto <emi.yamamoto73@gmail.com>
2019-06-22 17:37:11 +0900

    revised item show

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,5 @@ class Item < ApplicationRecord
   def next
     user.items.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
   end
-
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "items#index"
+  root to: "items#show"
   get 'users/sign_up' => 'users#sign_up'
   get 'users/sign_in' => 'users#sign_in'
   get 'users/registration_base' => 'users#registration_base'
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'items/new'
   get 'items/show'
+  resources :evaluations, only: :show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
